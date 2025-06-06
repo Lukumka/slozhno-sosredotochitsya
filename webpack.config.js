acf3cad52ca9b8f7d6eeaf69e26a4f0e90a9c062
Andrey Krivosheev <andreykrt12@gmail.com>
2025-06-06 20:24:51 +0300

    6.2

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,9 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'  // сохранять оригинальные имена без хеша
+                }
             },
 
         ]
